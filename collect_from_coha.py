7e6ede097b7ae9740af3af534372653cd017b4f2
glnmario <m.giulianelli.m@gmail.com>
2019-10-09 12:12:49 +0200

    Add more arguments from command line

--- collect_from_coha.py
+++ collect_from_coha.py
@@ -6,6 +6,8 @@ from usage_collector import collect_from_coha
 parser = argparse.ArgumentParser()
 parser.add_argument('--seqlen', type=list, default=[128, 256])
 parser.add_argument('--bertdir', type=str, default='models/bert-base-uncased')
+parser.add_argument('--cohadir', type=str, default='data/coha')
+parser.add_argument('--outdir', type=str, default='data')
 parser.add_argument('--buffer', type=int, default=1024)
 
 args = parser.parse_args()
@@ -26,8 +28,9 @@ for seq_len in args.seqlen:
                                decades,
                                sequence_length=seq_len,
                                pretrained_weights=args.bertdir,
+                               coha_dir=args.cohadir,
                                buffer_size=args.buffer)
 
     # Save usages
-    with open('data/usages_16_len{}.dict'.format(seq_len), 'wb') as f:
+    with open('{}/usages_16_len{}.dict'.format(args.outdir, seq_len), 'wb') as f:
         pickle.dump(usages, file=f)
