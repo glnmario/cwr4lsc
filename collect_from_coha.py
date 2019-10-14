fc2ecea9bb6a5c002f2461d9301bab73497d7415
glnmario <m.giulianelli.m@gmail.com>
2019-10-14 09:38:43 +0200

    Split extraction even more: decade by decade

--- collect_from_coha.py
+++ collect_from_coha.py
@@ -4,7 +4,7 @@ import numpy as np
 from usage_collector import collect_from_coha
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--seqlen', type=list, default=[128, 256])
+parser.add_argument('--seqlen', type=list, default=[128])
 parser.add_argument('--bertdir', type=str, default='models/bert-base-uncased')
 parser.add_argument('--cohadir', type=str, default='data/coha')
 parser.add_argument('--outdir', type=str, default='data')
@@ -17,31 +17,22 @@ targets = ['net', 'virtual', 'disk', 'card', 'optical', 'virus',
            'signal', 'mirror', 'energy', 'compact', 'leaf',
            'brick', 'federal', 'sphere', 'coach', 'spine']
 
+
 for seq_len in args.seqlen:
     print('{}\nSEQUENCE LENGTH: {}\n{}'.format('-'*30, seq_len, '-'*30))
 
-    decades = list(np.arange(1910, 2001, 10))
+    # decades = list(np.arange(1910, 2001, 10))
     # decades = list(np.arange(1810, 1811, 10))
 
-    usages = collect_from_coha(targets,
-                               list(np.arange(1910, 1959, 10)),
-                               sequence_length=seq_len,
-                               pretrained_weights=args.bertdir,
-                               coha_dir=args.cohadir,
-                               buffer_size=args.buffer)
+    for decade in np.arange(1960, 2009, 10):
+        usages = collect_from_coha(targets,
+                                   [decade],
+                                   sequence_length=seq_len,
+                                   pretrained_weights=args.bertdir,
+                                   coha_dir=args.cohadir,
+                                   buffer_size=args.buffer)
 
-    # Save usages
-    with open('{}/usages_16_len{}_1910-1950.dict'.format(args.outdir, seq_len), 'wb') as f:
-        pickle.dump(usages, file=f)
-    usages = None
-
-    usages = collect_from_coha(targets,
-                               list(np.arange(1960, 2009, 10)),
-                               sequence_length=seq_len,
-                               pretrained_weights=args.bertdir,
-                               coha_dir=args.cohadir,
-                               buffer_size=args.buffer)
-
-    # Save usages
-    with open('{}/usages_16_len{}_1960-2000.dict'.format(args.outdir, seq_len), 'wb') as f:
-        pickle.dump(usages, file=f)
+        # Save usages
+        with open('{}/usages_16_len{}_{}.dict'.format(args.outdir, seq_len, decade), 'wb') as f:
+            pickle.dump(usages, file=f)
+        usages = None
