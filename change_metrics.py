70b80548a3ba50d14013d7068318602ed7a7bd27
glnmario <m.giulianelli.m@gmail.com>
2019-11-27 18:03:39 +0100

    Update change metrics and use them for evaluation

--- change_metrics.py
+++ change_metrics.py
@@ -5,11 +5,14 @@ from sklearn import preprocessing
 from tqdm import tqdm
 
 
-def entropy_timeseries(usage_distribution):
+def entropy_timeseries(usage_distribution, intervals=None):
     """
     :param usage_distribution: a CxT diachronic usage distribution matrix
     :return: array of entropy values, one for each usage distribution
     """
+    if intervals:
+        usage_distribution = usage_distribution[:, intervals]
+
     usage_distribution = preprocessing.normalize(usage_distribution, norm='l1', axis=0)
 
     H = []
@@ -24,12 +27,15 @@ def entropy_timeseries(usage_distribution):
     return np.array(H)
 
 
-def entropy_difference_timeseries(usage_distribution):
+def entropy_difference_timeseries(usage_distribution, absolute=True, intervals=None):
     """
     :param usage_distribution: a CxT diachronic usage distribution matrix
     :return: array of entropy differences between contiguous usage distributions
     """
-    return np.array([abs(d) for d in np.diff(entropy_timeseries(usage_distribution))])
+    if absolute:
+        return np.array([abs(d) for d in np.diff(entropy_timeseries(usage_distribution, intervals))])
+    else:
+        return np.diff(entropy_timeseries(usage_distribution, intervals))
 
 
 def js_divergence(*usage_distribution):
@@ -52,12 +58,15 @@ def js_distance(*usage_distribution):
     return np.sqrt(js_divergence(usage_distribution))
 
 
-def jsd_timeseries(usage_distribution, dfunction=js_divergence):
+def jsd_timeseries(usage_distribution, dfunction=js_divergence, intervals=None):
     """
     :param usage_distribution: a CxT diachronic usage distribution matrix
     :param dfunction: a JSD function (js_divergence or js_distance)
     :return: array of JSD between contiguous usage distributions
     """
+    if intervals:
+        usage_distribution = usage_distribution[:, intervals]
+
     usage_distribution = preprocessing.normalize(usage_distribution, norm='l1', axis=0)
     distances = []
     for t in range(usage_distribution.shape[1] - 1):
@@ -73,7 +82,7 @@ def jsd_timeseries(usage_distribution, dfunction=js_divergence):
     return np.array(distances)
 
 
-def avg_pairwise_distance_timeseries(usages, metrics=('cosine', 'canberra')):
+def avg_pairwise_distance_timeseries(usages, metrics=('cosine', 'canberra'), interval_labels=None):
     """
     :param usages: 4-place data structure containing usage vectors, sentences, sentence positions, and time labels
     :param metrics: a list or tuple of metric names for scipy.spatial.distance.cdist
@@ -83,6 +92,8 @@ def avg_pairwise_distance_timeseries(usages, metrics=('cosine', 'canberra')):
 
     by_time = {}
     for u_w, sent, pos, t_label in zip(U_w, contexts, pos_in_context, t_labels):
+        if interval_labels and t_label not in interval_labels:
+            continue
         try:
             by_time[t_label] = np.vstack((by_time[t_label], u_w))
         except KeyError:
--- eval_change_metrics.py
+++ eval_change_metrics.py
@@ -4,7 +4,8 @@ import numpy as np
 from tqdm import tqdm
 from sklearn import preprocessing
 import pandas as pd
-from change_metrics import jsd_timeseries, avg_pairwise_distance_timeseries, entropy_difference_timeseries
+from change_metrics import jsd_timeseries, avg_pairwise_distance_timeseries, entropy_difference_timeseries, js_distance, \
+    js_divergence
 from clustering import usage_distribution
 
 
@@ -16,10 +17,27 @@ def gulordava_baroni_correlation(clusterings, usages, word2shift):
     :param word2shift: dictionary mapping target words to human-rated shift scores
     :return: dataframe containing measurements for all JSD, entropy difference, and average pairwise distance
     """
-    jsd = []
-    dh = []
-    avgcos = []
-    avgcan = []
+    jsd_multi = []
+    jsd_mean = []
+    dh_mean = []
+    jsd_max = []
+    dh_max = []
+    jsd_min = []
+    dh_min = []
+    jsd_median = []
+    dh_median = []
+    avgcos_mean = []
+    avgcan_mean = []
+    avgcos_max = []
+    avgcan_max = []
+    avgcos_min = []
+    avgcan_min = []
+    avgcos_median = []
+    avgcan_median = []
+    avgeuc_mean = []
+    avgeuc_max = []
+    avgeuc_min = []
+    avgeuc_median = []
     shift = []
 
     for word in tqdm(clusterings):
@@ -30,22 +48,73 @@ def gulordava_baroni_correlation(clusterings, usages, word2shift):
         usage_distr = usage_distribution(clustering.labels_, t_labels)
         usage_distr = preprocessing.normalize(usage_distr, norm='l1', axis=0)
 
-        print('jsd')
-        jsd.append(np.mean(jsd_timeseries(usage_distr)))
-        print('dh')
-        dh.append(np.mean(entropy_difference_timeseries(usage_distr)))
-        print('avg d')
-        avgd = avg_pairwise_distance_timeseries(usages[word], metrics=['cosine', 'canberra'])
-        avgcos.append(np.mean(avgd['cosine']))
-        avgcan.append(np.mean(avgd['canberra']))
+        intervals = [5, 6, 7, 8]
+        interval_labels = [1960, 1970, 1980, 1990]
+        # intervals = [5, 8]
+        # interval_labels = [1960, 1990]
+
+        # JSD
+        jsd = jsd_timeseries(usage_distr, dfunction=js_divergence, intervals=intervals) / usage_distr.shape[0]
+        jsd_multi.append(js_divergence([usage_distr[:, t] for t in intervals]))
+        jsd_mean.append(np.mean(jsd))
+        jsd_max.append(np.max(jsd))
+        jsd_min.append(np.min(jsd))
+        jsd_median.append(np.median(jsd))
+
+        # Entropy difference
+        dh = entropy_difference_timeseries(usage_distr, absolute=False, intervals=intervals) / usage_distr.shape[0]
+        dh_mean.append(np.mean(dh))
+        dh_max.append(np.max(dh))
+        dh_min.append(np.min(dh))
+        dh_median.append(np.median(dh))
+
+        # Average pairwise distance
+        avgd = avg_pairwise_distance_timeseries(
+            usages[word],
+            metrics=['cosine', 'canberra', 'euclidean'],
+            interval_labels=interval_labels
+        )
+        # cosine
+        avgcos_mean.append(np.mean(avgd['cosine']))
+        avgcos_max.append(np.max(avgd['cosine']))
+        avgcos_min.append(np.min(avgd['cosine']))
+        avgcos_median.append(np.median(avgd['cosine']))
+        # canberra
+        avgcan_mean.append(np.mean(avgd['canberra']))
+        avgcan_max.append(np.max(avgd['canberra']))
+        avgcan_min.append(np.min(avgd['canberra']))
+        avgcan_median.append(np.median(avgd['canberra']))
+        # euclidean
+        avgeuc_mean.append(np.mean(avgd['euclidean']))
+        avgeuc_max.append(np.max(avgd['euclidean']))
+        avgeuc_min.append(np.min(avgd['euclidean']))
+        avgeuc_median.append(np.median(avgd['euclidean']))
+
         shift.append(word2shift[word])
 
     df = pd.DataFrame(
         data={
-            'JSD': jsd,
-            'H diff': dh,
-            'cosine': avgcos,
-            'canberra': avgcan,
+            'JSD mean': jsd_mean,
+            'JSD max': jsd_max,
+            'JSD min': jsd_min,
+            'JSD median': jsd_median,
+            'JSD multi': jsd_multi,
+            'HD mean': dh_mean,
+            'HD max': dh_max,
+            'HD min': dh_min,
+            'HD median': dh_median,
+            'cosine mean': avgcos_mean,
+            'cosine max': avgcos_max,
+            'cosine min': avgcos_min,
+            'cosine median': avgcos_median,
+            'canberra mean': avgcan_mean,
+            'canberra max': avgcan_max,
+            'canberra min': avgcan_min,
+            'canberra median': avgcan_median,
+            'euclidean mean': avgeuc_mean,
+            'euclidean max': avgeuc_max,
+            'euclidean min': avgeuc_min,
+            'euclidean median': avgeuc_median,
             'shift': shift
         },
         index=list(clusterings.keys())
@@ -57,12 +126,12 @@ def gulordava_baroni_correlation(clusterings, usages, word2shift):
 if __name__ == '__main__':
     # Load usages
     # with open('coha/6090/sum/usages_len128_silhouette.dict', 'rb') as f:
-    with open('data/sum/usages_5_len128_all.dict', 'rb') as f:
+    with open('data/sum/usages_16_len128_all.dict', 'rb') as f:
         usages = pickle.load(f)
 
     # Load clusterings
     # with open('coha/6090/sum/usages_len128_silhouette.clustering.dict', 'rb') as f:
-    with open('data/sum/usages_5_len128_silhouette.clustering.dict', 'rb') as f:
+    with open('data/sum/usages_16_len128_silhouette.clustering.dict', 'rb') as f:
         clusterings = pickle.load(f)
 
     # Load shift scores from (Gulordava & Baroni, 2011)
